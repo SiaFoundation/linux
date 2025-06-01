576a6d5746010c0ce913e5e497d3a0817edc5ad9
Nate <me@n8m.us>
2025-06-01 16:11:39 -0700

    use combined embed directive

--- embed_test.go
+++ embed_test.go
@@ -8,21 +8,29 @@ import (
 )
 
 func TestPackagesFS(t *testing.T) {
-	expected, err := os.ReadFile("debian/gpg")
-	if err != nil {
-		t.Fatal(err)
+	assertFile := func(t *testing.T, path string) {
+		t.Helper()
+		expected, err := os.ReadFile(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		f, err := Packages.Open(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+
+		data, err := io.ReadAll(f)
+		if err != nil {
+			t.Fatal(err)
+		} else if !bytes.Equal(data, expected) {
+			t.Fatalf("expected %q, got %q", expected, data)
+		}
 	}
 
-	f, err := Packages.Open("/debian/gpg")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	data, err := io.ReadAll(f)
-	if err != nil {
-		t.Fatal(err)
-	} else if !bytes.Equal(data, expected) {
-		t.Fatalf("expected %q, got %q", expected, data)
-	}
+	assertFile(t, "debian/gpg")
+	assertFile(t, "ubuntu/gpg")
+	assertFile(t, "debian/db/checksums.db")
+	assertFile(t, "ubuntu/db/checksums.db")
 }
